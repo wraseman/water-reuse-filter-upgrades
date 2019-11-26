0d51a2ace73871fc2af4b8c2fd657b2f7a5a2978
Billy <wraseman@gmail.com>
2019-11-26 10:52:11 -0700

    Correct data entry errors for Sample IDs

--- R-new/01_read-clean_ca-labs.R
+++ R-new/01_read-clean_ca-labs.R
@@ -22,20 +22,23 @@ excel.array <- str_subset(file.array, "xlsx") %>%  # only include Excel spreadsh
   str_subset("Sample_ID_key.xlsx", negate=TRUE)  # don't keep Sample ID key file
 
 ca.df1 <- data.frame()  # empty dataframe for California data
-# for (i in 1:length(file.array )){
-#   temp.df <- read_excel(str_c(ca.dir1, excel.array[i]), range = cell_cols("A:AI"))
-#   ca.df1 <- rbind(ca.df1, temp.df)  # for each iteration, bind the new data to the building dataset
-# }
 
 ## read in data with specified columns
 col.types <- c("numeric", "text", "text", "numeric", "numeric", "text", "text", 
-              "numeric", "numeric", "date", "date", "date",	"date",	
-              "text",	"text",	"text",	"text",	"text",	"text",	
-              "numeric", 	"text",	"text",	"numeric",	"numeric",	
-              "numeric",	"numeric",	"numeric",	"numeric",	"numeric", 	
-              "date",	"date",	"date",	"date",	"text",	"text")  # specify column types
-ca.df1 <- read_excel(str_c(ca.dir, excel.array[1]), range = cell_cols("A:AI"),
-                      col_types = col.types)
+               "numeric", "numeric", "date", "date", "date",	"date",	
+               "text",	"text",	"text",	"text",	"text",	"text",	
+               "numeric", 	"text",	"text",	"numeric",	"numeric",	
+               "numeric",	"numeric",	"numeric",	"numeric",	"numeric", 	
+               "text",	"text",	"text",	"text",	"text",	"text")  # specify column types
+
+# ca.df1 <- read_excel(str_c(ca.dir, excel.array[9]), range = cell_cols("A:AI"),
+#                      col_types = col.types)
+
+for (i in 1:length(excel.array)){
+  temp.df <- read_excel(str_c(ca.dir, excel.array[i]), range = cell_cols("A:AI"),
+                        col_types = col.types)
+  ca.df1 <- rbind(ca.df1, temp.df)  # for each iteration, bind the new data to the building dataset
+}
 
 ## select only the columns that are needed and remove whitespace from column names
 selected.cols <- c("Sample ID", "Sample Date", "Sample Time", 
@@ -62,8 +65,27 @@ id.df <- read_excel(str_c(ca.dir, id.table))
 colnames(id.df) <- c("SampleID", "ClientID")
 id.df$SampleID <- as.character(id.df$SampleID)
 
-## join data with sample ID to get the "client ID" which contains location information and filter type
-ca.df5 <- left_join(ca.df4, id.df)  # add sample location (raw data) using Sample ID key
+## join data with Sample ID to get the "client ID" which contains location information and filter type
+### account for errors in Eurofins data entry for Sample ID
+id.errors <- c("2885588", "3124086", "3124087")
+id.corrections <- c("3885588", "3421086", "3421087")
+
+ca.df5 <- mutate(ca.df4, SampleID = if_else(SampleID==id.errors[1], id.corrections[1],
+                                            if_else(SampleID==id.errors[2], id.corrections[2],
+                                                    if_else(SampleID==id.errors[3], id.corrections[3],
+                                              SampleID))))
+
+### check that Sample IDs are correct
+#### for all 7 digit Sample IDs, check that they match the Sample ID key options
+ids.7dig <- mutate(ca.df5, SampleIDLength = str_length(SampleID)) %>%
+  filter(SampleIDLength == 7) %>%
+  select(SampleID) %>% unique %>% unlist
+
+check.ids <- (ids.7dig %in% id.df$SampleID) %>%
+  all(TRUE)  # confirmed that all 7 digit Sample ID now match those in the key table
+
+### add sample location (raw data) using Sample ID key
+ca.df5 <- left_join(ca.df5, id.df)  
 
 ## identify duplicate and triplicate samples and extract location
 duplicates <- c("Final-2", "DBF-E-2", "DBF-I-2", 
